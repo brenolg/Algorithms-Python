057c4092bc405c881f3f31d67e9a0ba86ac12800
brenolg <brenolg@yahoo.com.br>
2023-06-04 18:30:19 -0300

    feat: req 1 - Número de estudantes estudando no mesmo horário (Algoritmo de busca)

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    people_counter = 0
+
+    try:
+        for student in permanence_period:
+            if student[0] <= target_time <= student[1]:
+                people_counter += 1
+    except (ValueError, TypeError):
+        return None
+    return people_counter
